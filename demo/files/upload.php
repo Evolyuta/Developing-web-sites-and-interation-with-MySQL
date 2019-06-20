5d67c45f1b545ef1568740068a375b145c881533
Evolyuta <dima.97_97@mail.ru>
2019-06-20 10:46:18 +0300

    add upload dir and hidden input

--- demo/files/upload.php
+++ demo/files/upload.php
@@ -2,13 +2,16 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    var_dump($_FILES);
+//    var_dump($_FILES);
     $n = $_FILES["userfile"]["name"];
     $t = $_FILES["userfile"]["tmp_name"];
-    move_uploaded_file($t, $n);
+    if (!is_dir("upload"))
+        mkdir("upload");
+    move_uploaded_file($t, "upload/" . $n);
 }
 ?>
 <form action='upload.php' method='post' enctype='multipart/form-data'>
-<input type='file' name='userfile'>
-<input type='submit'>
+<!--    <input type="hidden" name="MAX_FILE_SIZE" value="4096">-->
+    <input type='file' name='userfile'>
+    <input type='submit'>
 </form>
