457d6cc0beca0c266244583adfde462a933f8790
Evolyuta <dima.97_97@mail.ru>
2019-06-25 11:31:11 +0300

    implement functions of adding order into db

--- eshop/admin/save2cat.php
+++ eshop/admin/save2cat.php
@@ -1,5 +1,19 @@
 <?php
-	// подключение библиотек
-	require "secure/session.inc.php";
-	require "../inc/lib.inc.php";
-	require "../inc/config.inc.php";
+// подключение библиотек
+require "secure/session.inc.php";
+require "../inc/lib.inc.php";
+require "../inc/config.inc.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = clearStr($_POST['title']);
+    $author = clearStr($_POST['author']);
+    $pubyear = (int)(abs($_POST['pubyear']));
+    $price = (int)(abs($_POST['price']));
+
+    if (!addItemToCatalog($title, $author, $pubyear, $price)) {
+        echo 'Произошла ошибка при добавление товара в каталог';
+    } else {
+        header("Location: add2cat.php");
+        exit;
+    }
+}
