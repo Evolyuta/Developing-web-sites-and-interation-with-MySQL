8a4ba458d88925fc2a7e6a9bef82903d1419fc25
Evolyuta <dima.97_97@mail.ru>
2019-06-25 14:36:53 +0300

    add myBasket, resultToArray functions

--- eshop/inc/lib.inc.php
+++ eshop/inc/lib.inc.php
@@ -57,6 +57,38 @@ function basketInit()
 function addToBasket($id)
 {
     global $basket;
+//    if (!isset($basket[$id]))
     $basket[$id] = 1;
+//    else
+//        $basket[$id] += 1;
     saveBasket();
+}
+
+function myBasket()
+{
+    global $link, $basket;
+
+    $goods = array_keys($basket);
+    array_shift($goods);
+    if (!$goods) return false;
+
+    $ids = implode(",", $goods);
+
+    $sql = "SELECT id, author, title, pubyear, price FROM catalog WHERE id IN ($ids)";
+    if (!$result = mysqli_query($link, $sql)) return false;
+
+    $items = resultToArray($result);
+    mysqli_free_result($result);
+    return $items;
+}
+
+function resultToArray($data)
+{
+    global $basket;
+    $arr = [];
+    while ($row = mysqli_fetch_assoc($data)) {
+        $row['quantity'] = $basket[$row['id']];
+        $arr[] = $row;
+    }
+    return $arr;
 }
